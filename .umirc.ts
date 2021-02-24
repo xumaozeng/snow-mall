f1214debf63acf13d91b5758479edcbf9323c4e0
xumaozeng <15611895341@163.com>
2021-02-24 23:14:09 +0800

    新增cart,olist,user,login页

--- .umirc.ts
+++ .umirc.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'umi';
 
 export default defineConfig({
+  // 编译提速
   nodeModulesTransform: {
     type: 'none',
   },
+  // 路由配置
   routes: [
     { path: '/', component: '@/pages/index' },
+    { path: '/cart', component: '@/pages/cart/index' },
+    { path: '/login', component: '@/pages/login/index' },
+    { path: '/olist', component: '@/pages/olist/index' },
+    { path: '/user', component: '@/pages/user/index' },
   ],
   fastRefresh: {},
 });
