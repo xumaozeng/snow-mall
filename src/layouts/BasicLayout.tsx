c3438285f83279007e11de9d87826114da9831f5
xumaozeng <15611895341@163.com>
2021-03-08 18:00:34 +0800

    创建底部导航栏

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -1,15 +1,25 @@
 import BottomNav from '@/components/BottomNav';
 import React, { useEffect } from 'react';
+import '@/static/iconfont/iconfont.css';
+import { Location } from 'umi';
+import styles from './BasicLayout.less';
 
-interface BasicLayoutProps {}
+interface BasicLayoutProps {
+  location: Location;
+}
 
 const BasicLayout: React.FC<BasicLayoutProps> = (props) => {
-  const { children } = props;
+  const { children, location } = props;
+  const { pathname } = location;
+
   useEffect(() => {}, []);
+
   return (
-    <div>
+    <div className={styles.main}>
       <article>{children}</article>
-      <BottomNav />
+      <footer>
+        <BottomNav pathname={pathname} />
+      </footer>
     </div>
   );
 };
