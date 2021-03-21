f3782b16703d6ffa853353a925f69bb54c262436
xumaozeng <15611895341@163.com>
2021-03-21 23:02:47 +0800

    商品搜索页

---
+++ src/pages/search/index.tsx
@@ -0,0 +1,8 @@
+import React, { useEffect } from 'react';
+
+interface SearchProps {}
+const Search: React.FC<SearchProps> = (props) => {
+  useEffect(() => {}, []);
+  return <div></div>;
+};
+export default Search;
