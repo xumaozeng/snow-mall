8d41e3c476af4935ef6afe459c773e377b89e36d
xumaozeng <15611895341@163.com>
2021-03-28 18:29:27 +0800

    订单列表

--- src/pages/olist/index.tsx
+++ src/pages/olist/index.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import styles from './index.less';
+import { CartProductType } from '@/@types/product';
+import { query } from '@/services/olist';
+import { WhiteSpace, WingBlank } from 'antd-mobile';
+import { Component } from 'react';
+import List from './List';
 
-export default function Page() {
-  return (
-    <div>
-      <h1 className={styles.title}>Page olist/index</h1>
-    </div>
-  );
+interface OListState {
+  data: CartProductType[];
 }
+class OList extends Component<{}, OListState> {
+  state: OListState = {
+    data: [],
+  };
+
+  componentDidMount() {
+    query().then((res) => {
+      this.setState({ data: res.list.data });
+    });
+  }
+  render() {
+    const { data } = this.state;
+    return (
+      <WingBlank size="lg">
+        <WhiteSpace size="lg" />
+        <List data={data} />
+      </WingBlank>
+    );
+  }
+}
+export default OList;
